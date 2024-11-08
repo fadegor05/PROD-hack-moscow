5fa1ab891a08a5be8f8b7663bd359e82350cd6eb
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-09 00:22:20 +0300

    created user schema

--- app/api/v1/health_check/health_check.py
+++ app/api/v1/health_check/health_check.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 health_check_router = APIRouter(prefix="/health_check", tags=["Health Check"])
 
+
 @health_check_router.get("")
 async def get_health_check():
     return {"service": "healthy"}
