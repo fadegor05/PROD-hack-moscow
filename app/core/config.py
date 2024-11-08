ad938a96fffaee7cbc9c6845f47c95b12ae1c972
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-08 22:31:55 +0300

    added database connection

--- app/core/config.py
+++ app/core/config.py
@@ -1,21 +1,17 @@
-from pydantic_settings import BaseSettings
 from pydantic import computed_field
+from pydantic_settings import BaseSettings
 
 
 class PostgresSettings(BaseSettings):
-    """PostgreSQL connection settings."""
-    
     postgres_port: str
     postgres_user: str
     postgres_password: str
     postgres_db: str
-    
+
     @computed_field
     @property
     def postgres_url(self) -> str:
         return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@postgres:{self.postgres_port}/{self.postgres_db}"
-    
+
     class Config:
         extra = "allow"
-
-
