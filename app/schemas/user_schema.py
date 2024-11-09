c1ebb9097f31aad256a2125533d6e82979585096
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-09 11:54:17 +0300

    fixed user schema and crud

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,14 +1,10 @@
 from uuid import UUID
 
-from pydantic import ConfigDict
-
 from app.models.user_model import UserBase
 
 
 class IUserCreate(UserBase):
-    model_config = ConfigDict()
     password: str
-    hashed_password: str | None = None
 
 
 class IUserUpdate(UserBase):
